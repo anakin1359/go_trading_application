27b3e15da4cf4599bf898c68ac3a13a95de5dcf0
anakin1359 <anakin1359@gmail.com>
2022-04-22 02:16:56 +0900

    Setting Go Trading Application config

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
+type ConfigList struct {
+	ApiKey    string
+	ApiSecret string
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Printf("Failed to read file: %v", err)
+		os.Exit(1) // configが見つからなかった場合はエラーコード1で抜ける
+	}
+
+	Config = ConfigList{
+		ApiKey:    cfg.Section("bitflyer").Key("api_key").String(),
+		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gotrading
+
+go 1.18
+
+require gopkg.in/go-ini/ini.v1 v1.66.4 // indirect
